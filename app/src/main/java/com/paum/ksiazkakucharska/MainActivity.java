975aa4b183571e10c96cedab3e2cd9c08b5d9700
Mirti <avalith@o2.pl>
2017-12-01 21:53:01 +0100

    Dodany szkielet menu i intenty

--- app/src/main/java/com/paum/ksiazkakucharska/MainActivity.java
+++ app/src/main/java/com/paum/ksiazkakucharska/MainActivity.java
@@ -1,7 +1,9 @@
 package com.paum.ksiazkakucharska;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startRecipesActivity(View view){
+        Intent intent = new Intent(this, RecipesActivity.class);
+        startActivity(intent);
+    }
+
+    public void startSearchActivity(View view){
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void startAddRecipeActivity(View view){
+        Intent intent = new Intent (this, AddRecipeActivity.class);
+        startActivity(intent);
+    }
+
+    public void startListsActivity(View view){
+        Intent intent = new Intent(this, ListsActivity.class);
+        startActivity(intent);
     }
 }
